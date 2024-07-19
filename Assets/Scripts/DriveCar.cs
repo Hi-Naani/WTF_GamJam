0648dde82fb8f7e4a69bb3deb9526579f8ddfef8
{Poorv} <poorvpatidar2112@gmail.com>
2024-07-20 02:33:44 +0530

    Organized Project

--- Assets/Scripts/DriveCar.cs
+++ Assets/Scripts/DriveCar.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class DriveCar : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] Rigidbody2D frb;
-    [SerializeField] Rigidbody2D brb;
-    [SerializeField] Rigidbody2D carrb;
+    [SerializeField] Rigidbody2D frontRb;
+    [SerializeField] Rigidbody2D backRb;
+    [SerializeField] Rigidbody2D carRb;
     [SerializeField] float speed = 150f;
     [SerializeField] float rotSpeed = 300f;
     float moveInput;
@@ -17,8 +17,8 @@ public class DriveCar : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        frb.AddTorque(-moveInput * speed * Time.deltaTime);
-        brb.AddTorque(-moveInput * speed * Time.deltaTime);
-        carrb.AddTorque(moveInput * rotSpeed * Time.deltaTime);
+        frontRb.AddTorque(-moveInput * speed * Time.deltaTime);
+        backRb.AddTorque(-moveInput * speed * Time.deltaTime);
+        carRb.AddTorque(moveInput * rotSpeed * Time.deltaTime);
     }
 }
