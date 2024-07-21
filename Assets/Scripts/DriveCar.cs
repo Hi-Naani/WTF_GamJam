94248f8cad9220ab53576f62b55d1d19e343802c
{Poorv} <poorvpatidar2112@gmail.com>
2024-07-21 19:49:58 +0530

    c

--- Assets/Scripts/DriveCar.cs
+++ Assets/Scripts/DriveCar.cs
@@ -58,10 +58,6 @@ public class DriveCar : MonoBehaviour
         {
             UpdateRespawnPoint();
         }
-        if (collision.gameObject.layer == 7) // Finish Line Layer
-        {
-            this.GetComponent<DriveCar>().enabled = false;
-        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
